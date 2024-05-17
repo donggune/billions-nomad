a5ce7da90eb2c40503f365fa39b920a37af9a0df
lion <rhkwkdkrl@gmail.com>
2024-05-18 01:17:15 +0900

    Assignment 4

---
+++ app/person/[id]/page.tsx
@@ -0,0 +1,36 @@
+import { formatNetWorth } from "@/app/page";
+import Image from "next/image";
+
+function getPerson(id: string) {
+  return fetch(`https://billions-api.nomadcoders.workers.dev/person/${id}`).then((res) => res.json());
+}
+
+export default async function PersonPage({ params }: { params: { id: string } }) {
+  const person = await getPerson(params.id);
+  return (
+    <div className="max-w-screen-lg m-auto">
+      <div className="my-16 bg-gray-800 p-8 py-20">
+        <Image src={person.squareImage} alt={person.name} width={416} height={416} />
+        <p className="font-bold text-2xl mt-5">{person.name}</p>
+        <div className="mt-5 flex flex-col gap-2">
+          <p>Networth: {formatNetWorth(person.netWorth)}</p>
+          <p>Country: {person.country}</p>
+          <p>Industry: {person.industries}</p>
+          <p>{person.bio}</p>
+        </div>
+      </div>
+      <div className="my-16 bg-gray-800 p-8 py-20">
+        <h1 className="text-2xl font-bold">Financial Assets</h1>
+        <div className="mt-5 grid grid-cols-4 gap-4">
+          {person.financialAssets.map((asset: any) => (
+            <div key={asset.id} className="border border-gray-600 p-4 rounded-md">
+              <p>Ticekr: {asset.ticker}</p>
+              <p>Shares: {asset.numberOfShares.toLocaleString()}</p>
+              {asset.exerciseOptionPrice && <p>Excersie Price: ${asset.exerciseOptionPrice}</p>}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["specials-images.forbesimg.com"],
+  },
+};
 
 export default nextConfig;
